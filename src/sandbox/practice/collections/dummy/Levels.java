c72b5a3f99f3cc05818444ab5d18a79d40842c3e
Habib Shaikh <habib.shaik@telintel.net>
2024-01-13 21:12:20 +0530

    Removed Practice Materials

--- src/sandbox/practice/collections/dummy/Levels.java
+++
@@ -1,5 +0,0 @@
-package sandbox.practice.collections.dummy;
-
-public enum Levels {
-    HOTFIX, HIGH, TOP, MAJOR, COMMON, AVERAGE, LOW, MINOR
-}
--- src/sandbox/practice/collections/dummy/Powers.java
+++
@@ -1,5 +0,0 @@
-package sandbox.practice.collections.dummy;
-
-public enum Powers {
-    INVISIBILITY, SPEED, ELASTICITY, STRENGTH, SPEECH,
-}
