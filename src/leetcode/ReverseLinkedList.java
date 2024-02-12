0d0f816540be852ff6dc3014b32dbdf898ac1b6f
Habib Shaikh <habib.shaik@telintel.net>
2024-02-12 19:35:55 +0530

    Day 8

--- src/leetcode/ReverseLinkedList.java
+++ src/leetcode/ReverseLinkedList.java
@@ -5,26 +5,18 @@ import leetcode.helper.classes.ListNode;
 public class ReverseLinkedList {
   public static void main(String[] args) {
     ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, null))));
-    System.out.println(reverseList(l1).getVal());
+    System.out.println(reverseList(l1));
   }
 
   public static ListNode reverseList(ListNode head) {
     ListNode last = head;
-    if (head == null) {
-      return head;
+    ListNode prev = null;
+    while (last != null) {
+      ListNode next = last.getNext();
+      last.setNext(prev);
+      prev = last;
+      last = next;
     }
-    while (last.getNext() != null) {
-      last = last.getNext();
-    }
-    doReversal(head);
-    head.setNext(null);
     return last;
-  }
-
-  private static ListNode doReversal(ListNode curr) {
-    if (curr.getNext() != null) {
-      doReversal(curr.getNext()).setNext(curr);
-    }
-    return curr;
   }
 }
